4fcfb834776a30bea83e40aa7c3f85f750ba6e7b
Vasilkevich Ilya <Vasilkevich@redsoft.ru>
2020-10-03 23:58:49 +0300

    feat: what-season complete

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+
+  // if (typeof date.getMonth !== 'function') throw new Error();
+  if (Object.prototype.toString.call(date) !== "[object Date]") throw new Error();
+
+  const month = date.getMonth();
+  // return month;
+
+  if (month >= 2 && month <= 4) {
+    return 'spring';
+  } else if (month >= 5 && month <= 7) {
+    return 'summer';
+  } else if (month >= 8 && month <= 10) {
+    return 'fall';
+  } else {
+    return 'winter';
+  }
 };
