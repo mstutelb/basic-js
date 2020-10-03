dc21809c4fb72c6b1db04e280e85dd16527c0fa7
Vasilkevich Ilya <Vasilkevich@redsoft.ru>
2020-10-03 22:49:04 +0300

    feat: carbon-dating complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,17 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity !== 'string' ||
+    !isFinite(sampleActivity) ||
+    +sampleActivity <= 0
+  )
+    return false;
+
+  return Math.round(
+    Math.log(MODERN_ACTIVITY / +sampleActivity) / HALF_LIFE_PERIOD
+  );
 };
