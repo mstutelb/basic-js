adc860058b21fe2a634da24dcaa05930ae52438e
Vasilkevich Ilya <Vasilkevich@redsoft.ru>
2020-10-03 22:17:46 +0300

    feat: count-cats complete

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
   // remove line with error and write your code here
+
+  let count = 0;
+
+  matrix.forEach((line) => {
+    line.forEach((item) => {
+      count += item === '^^' ? 1 : 0;
+    });
+  });
+
+  return count;
 };
