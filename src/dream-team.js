7285f323fd69d4fefb703289c45e93f27e5ac598
Vasilkevich Ilya <Vasilkevich@redsoft.ru>
2020-10-03 23:08:09 +0300

    feat: dream-team complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let result = [];
+
+  members.forEach((member) => {
+    if(typeof member !== 'string') return
+    result.push( member.trim().slice(0,1).toUpperCase());
+  });
+
+  result = result.sort().join('');
+  
+  return result;
 };
