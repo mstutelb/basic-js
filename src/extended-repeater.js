16189eadca9008e2c2d234c0cd15316edc775926
Vasilkevich Ilya <Vasilkevich@redsoft.ru>
2020-10-04 20:29:10 +0300

    feat: repeater compelete

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  let addition;
+  if (options.addition !== undefined) {
+    addition = options.addition + '' || '';
+  }
+  const additionSeparator = options.additionSeparator || '|';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  let addSeporation = Array(additionRepeatTimes).fill(addition);
+  addSeporation = addSeporation.join(additionSeparator);
+
+  const result = Array(repeatTimes).fill(str + addSeporation);
+
+  return result.join(separator);
 };
-  
