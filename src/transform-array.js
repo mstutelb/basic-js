9f4528e2282b050e1e2d8710e26d895d88118d61
Vasilkevich Ilya <Vasilkevich@redsoft.ru>
2020-10-04 15:34:42 +0300

    feat: chain complete

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'string') {
+      result.push(arr[i]);
+    } else {
+      switch (arr[i]) {
+        case '--discard-next':
+          i++;
+          break;
+        case '--discard-prev':
+          if (arr[i - 2] !== '--discard-next') {
+            result.pop();
+          }
+          break;
+        case '--double-next':
+          if (i + 1 < arr.length) {
+            result.push(arr[i + 1]);
+          }
+          break;
+        case '--double-prev':
+          if (result.length > 0 && arr[i - 2] !== '--discard-next') {
+            result.push(arr[i - 1]);
+          }
+          break;
+        default:
+          result.push(arr[i]);
+      }
+    }
+  }
+
+  return result;
 };
